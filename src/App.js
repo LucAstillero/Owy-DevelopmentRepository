8c2e88f62abcb85a2ff7e30f1197d165cff54882
L. Ast. <90859217+LucAstillero@users.noreply.github.com>
2025-07-14 19:27:08 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <div className="main-container">
+      <div className="pic" />
+      <span className="text">Owy </span>
+
+      <div className="group">
+        <div className="group-2" />
+        <span className="text-2">Hola, Raquel</span>
+      </div>
+
+      <span className="text-3">¿En qué puedo ayudarte?</span>
+      <div className="img" />
+
+      <div className="section">
+        <div className="section-2">
+          <div className="img-2" />
+          <span className="text-4">¿Cómo puedes ayudarme?</span>
+        </div>
+
+        <div className="group-3">
+          <div className="pic-2" />
+          <span className="text-5">Hazme un plan de acción para</span>
+        </div>
+
+        <div className="section-3">
+          <div className="img-3" />
+          <div className="wrapper">
+            <span className="text-6">¿Cómo puedo hacer </span>
+            <span className="text-7">mejorar mi pensamiento crítico?</span>
+          </div>
+        </div>
+
+        <div className="img-4" />
+        <span className="text-8">llegar a la entrega</span>
+      </div>
+
+      <div className="section-4">
+        <div className="wrapper-2">
+          <span className="text-9">¡Escríbelo!</span>
+          <div className="pic-3" />
+          <div className="pic-4" />
+          <div className="pic-5" />
+          <div className="img-5" />
+        </div>
+        <div className="pic-6" />
+      </div>
+
+      <span className="text-a">
+        *Owy y ChatGPT pueden cometer errores, revisa siempre la información.
+      </span>
+
+      <div className="img-6" />
+
+      <div className="section-5">
+        <span className="text-b">?</span>
+        <div className="img-7" />
+      </div>
+
+      <div className="group-4">
+        <div className="section-6">
+          <span className="text-c">DAI</span>
+        </div>
+        <div className="pic-7" />
+        <div className="pic-8" />
+        <span className="text-d">Grupo 1.01</span>
+      </div>
+
+      <span className="text-e">DAI - Dibujo Arquitectónico I</span>
+    </div>
+  );
+}
